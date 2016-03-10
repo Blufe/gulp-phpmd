d4d34969b8cd92361c9a819379cd9b97e73f575b
Blufe <bluephoenixlab@gmail.com>
2016-03-10 18:33:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# gulp-phpmd [![NPM version](https://badge.fury.io/js/gulp-phpmd.png)](https://www.npmjs.org/package/gulp-phpmd)
+
+> A gulp plugin for running [PHP Mess Detector](https://github.com/squizlabs/PHP_CodeSniffer).
+
+Derivative work of Dmitriy S. Simushev's [gulp-phpcs](https://github.com/JustBlackBird/gulp-phpcs)
+##Requirements
+ - [PHP Mess Detector](https://github.com/squizlabs/php_codesniffer#installation)
+
+##Installation
+```shell
+npm install gulp-phpmd --save-dev
+```
+
+## Usage
+
+```js
+var gulp = require('gulp');
+var phpmd = require('gulp-phpmd');
+
+gulp.task('default', function () {
+    return gulp.src(['src/**/*.php', '!src/vendor/**/*.*'])
+        // Validate code using PHP Mess Detector
+        .pipe(phpmd({
+            bin: 'src/vendor/bin/phpmd',
+            format: 'text',
+            ruleset: 'unusedcode',
+        }))
+        .on('error', console.error)
+});
+```
+
+
+## API
+
+### phpmd(options)
+
+#### options.bin
+
+Type: `String`
+
+Default: `'phpmd'`
+
+PHP Mess Detector executable.
+
+#### options.ruleset
+
+Type: `String`
+
+The format of the report, for multiple formats just use a comma separated
+string.
+
+#### options.ruleset
+
+Type: `String`
+
+The ruleset to check against
+
+#### options.minimumpriority
+
+Type: `String`
+
+pass --mininumpriority to phpmd
+
+#### options.strict
+
+Type: `String`
+
+pass --strict to phpmd
+
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT) © Higashi Ryohei
---
+++ index.js
@@ -0,0 +1,67 @@
+var gutil = require('gulp-util')
+var through = require('through2')
+var exec = require('child_process').exec
+
+/**
+ * Builds shell command for PHPMD according to specified options.
+ */
+var buildCommand = function(file, opt) {
+  var opt = opt || {}
+  var command = (opt.bin || 'phpmd') + ' ' + file.path
+
+  if (opt.hasOwnProperty('format')) {
+    command += ' ' + opt.format + ' '
+  }
+
+  if (opt.hasOwnProperty('ruleset')) {
+    command += ' ' + opt.ruleset + ' '
+  }
+
+  if (opt.hasOwnProperty('minimumpriority')) {
+    command += ' --minimumpriority="' + opt.minimumpriority + '"'
+  }
+
+  if (opt.hasOwnProperty('strict')) {
+    command += ' --strict'
+  }
+
+  return command
+}
+
+var phpmdPlugin = function(options) {
+  return through.obj(function(file, enc, callback) {
+    var stream = this
+
+    if (file.isNull()) {
+      stream.push(file)
+      callback()
+
+      return
+    }
+
+    if (file.isStream()) {
+      stream.emit('error', new gutil.PluginError('gulp-phpmd', 'Streams are not supported'))
+      callback()
+
+      return
+    }
+
+    // Run PHPMD
+    exec(buildCommand(file, options), function(error, stdout, stderr) {
+      if (error) {
+        var message = 'PHP Code Sniffer found a ' + chalk.yellow('problem')
+                    + ' in ' + chalk.magenta(file.path) + '\n\t'
+                    + report.output.replace(/\n/g, '\n\t');
+        stream.emit('error', new gutil.PluginError('gulp-phpmd', message))
+        callback()
+
+        return
+      }
+
+      stream.push(file)
+      callback()
+    })
+  })
+}
+
+module.exports = phpmdPlugin
